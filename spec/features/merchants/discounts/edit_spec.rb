6610801ce33b7e4633863d4682436eeec6bc0d01
Matt Toensing <matthew.toensing@gmail.com>
2021-08-06 21:39:18 -0700

    Remove user story comment

--- spec/features/merchants/discounts/edit_spec.rb
+++ spec/features/merchants/discounts/edit_spec.rb
@@ -25,15 +25,6 @@ RSpec.describe 'edit discount information' do
       expect(page).to_not have_content(10)
       expect(page).to have_content(20)
       expect(page).to have_content(discount.quantity_threshold)
-      # As a merchant
-      # When I visit my bulk discount show page
-      # Then I see a link to edit the bulk discount
-      # When I click this link
-      # Then I am taken to a new page with a form to edit the discount
-      # And I see that the discounts current attributes are pre-populated in the form
-      # When I change any/all of the information and click submit
-      # Then I am redirected to the bulk discount's show page
-      # And I see that the discount's attributes have been updated
     end
   end
 end
