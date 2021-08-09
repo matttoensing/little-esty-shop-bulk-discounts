98802ff1f649e7dd1db2535fe3c88df1e0e5e83a
Matt Toensing <matthew.toensing@gmail.com>
2021-08-09 08:34:09 -0700

    Add tests for discount name on pages

--- spec/features/merchants/discounts/new_spec.rb
+++ spec/features/merchants/discounts/new_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'new merchant discount page' do
       merchant = create(:merchant)
       visit new_merchant_discount_path(merchant.id)
 
+      fill_in :name, with: 'Labor Day Sale'
       fill_in :percentage, with: 20
       fill_in :quantity_threshold, with: 10
       click_on 'Submit'
@@ -13,6 +14,8 @@ RSpec.describe 'new merchant discount page' do
       new_discount = Discount.last
 
       expect(current_path).to eq(merchant_discounts_path(merchant.id))
+
+      expect(page).to have_content('Labor Day Sale')
       expect(page).to have_content(new_discount.percentage.to_i)
       expect(page).to have_content(new_discount.quantity_threshold)
     end
