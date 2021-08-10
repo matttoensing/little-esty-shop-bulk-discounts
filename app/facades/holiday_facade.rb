c8d4a2617412b674e7e5fc6242a7999ab2a4a129
Matt Toensing <matthew.toensing@gmail.com>
2021-08-10 15:08:06 -0700

    Change block name

--- app/facades/holiday_facade.rb
+++ app/facades/holiday_facade.rb
@@ -2,8 +2,8 @@ class HolidayFacade
   def self.holidays
     json = DateSwaggerService.next_three_holidays
 
-    holidays = json.map do |member_data|
-      Holiday.new(member_data)
+    holidays = json.map do |holiday_info|
+      Holiday.new(holiday_info)
     end
   end
 end
