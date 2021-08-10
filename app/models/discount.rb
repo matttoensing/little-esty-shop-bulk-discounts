f45435cd7a3da92fe07e988eb4686cd16cf4ec4c
Matt Toensing <matthew.toensing@gmail.com>
2021-08-09 20:18:14 -0700

    Create validations for discount

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,8 +1,7 @@
 class Discount < ApplicationRecord
   belongs_to :merchant
 
-  # validates :percentage, numericality: { greater_than: 0, less_than_or_equal_to: 100, only_integer: true}
-  # validates :quantity_threshold, numericality: { greater_than: 0, only_integer: true}
-  #
-  # scope :percentage, -> {:percentage / 100}
+  validates :name, presence: true
+  validates :percentage, numericality: { greater_than: 0, less_than_or_equal_to: 100, only_integer: true}
+  validates :quantity_threshold, numericality: { greater_than: 0, only_integer: true}
 end
--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -5,8 +5,9 @@ RSpec.describe Discount do
     it { should belong_to(:merchant) }
   end
 
-  # describe 'validations' do
-  #   it { should validate_presence_of(:percentage) }
-  #   it { should validate_presence_of(:quantity_threshold) }
-  # end
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_numericality_of(:percentage) }
+    it { should validate_numericality_of(:quantity_threshold) }
+  end
 end
