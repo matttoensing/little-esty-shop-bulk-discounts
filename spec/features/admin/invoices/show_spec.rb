e8d60daaeb639cdadb6c1138ad53305cffff9c73
Matt Toensing <matthew.toensing@gmail.com>
2021-08-10 17:00:49 -0700

    Include test for showing it will only update when admin selects invoice as completed

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -139,6 +139,41 @@ RSpec.describe 'Admin Invoice Show Page' do
           expect(page).to have_content("Discount: #{invoice_item2.discount}")
         end
       end
+
+      it 'will not save a value if the select field in not selected as complete' do
+        merchant = create(:merchant)
+        discount = create(:discount, merchant: merchant)
+        item1 = create(:item, merchant: merchant, unit_price: 20)
+        item2 = create(:item, merchant: merchant, unit_price: 45)
+        customer = create(:customer)
+        invoice = create(:invoice, customer: customer, status: 1)
+        transaction1 = create(:transaction, invoice: invoice)
+        transaction2 = create(:transaction, invoice: invoice)
+        invoice_item1 = create(:invoice_item, item: item1, invoice: invoice, unit_price: 20, quantity: 10)
+        invoice_item2 = create(:invoice_item, item: item2, invoice: invoice, unit_price: 45, quantity: 5)
+
+        visit admin_invoice_path(invoice.id)
+
+        within "#item-#{item1.id}" do
+          expect(page).to have_content("Discount: 0")
+        end
+
+        within "#item-#{item2.id}" do
+          expect(page).to have_content("Discount: #{invoice_item2.discount}")
+        end
+
+        page.select 'cancelled', from: "invoice[status]"
+
+        click_on "Submit"
+
+        within "#item-#{item1.id}" do
+          expect(page).to have_content("Discount: 0")
+        end
+
+        within "#item-#{item2.id}" do
+          expect(page).to have_content("Discount: 0")
+        end
+      end
     end
   end
 end
